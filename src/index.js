c8fe665b76e767cb2a901bb550c42202ddf123e1
courserav <lvsmlit@gmail.com>
2021-08-03 02:28:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  console.log("The DOM has loaded")
 });
+
+
+let taskForm = document.getElementById("create-task-form")
+let taskList = document.getElementById("tasks")
+
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault()
+  let text = document.getElementById("new-task-description")
+  taskList.innerHTML += `<li class="task-item">${text.value} <input type=button class="done-button" value="Done"></button></li>`
+
+  let doneButtons = document.getElementsByClassName("done-button")
+
+  for (let i = 0; i < doneButtons.length; i++){
+    doneButtons[i].addEventListener("click", function(e){
+      e.preventDefault()
+      this.parentElement.remove()
+    }, false)
+  }  
+  
+}, false)
+
+
